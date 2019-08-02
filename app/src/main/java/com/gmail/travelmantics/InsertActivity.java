d09557a2150a67786b33c06436897869e145d231
Marwan <emef4z@gmail.com>
2019-08-02 14:03:18 +0100

    Add save menu

--- app/src/main/java/com/gmail/travelmantics/InsertActivity.java
+++ app/src/main/java/com/gmail/travelmantics/InsertActivity.java
@@ -2,12 +2,21 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-public class InsertActivity extends AppCompatActivity {
+ public class InsertActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
     }
-}
+
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu) {
+         MenuInflater inflater = getMenuInflater();
+         inflater.inflate(R.menu.save_menu,menu);
+         return true;
+     }
+ }
